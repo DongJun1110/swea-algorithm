808210479e0847640f8572bb775db9f15518a021
cckim <r2222911r@ajou.ac.kr>
2023-12-31 11:35:17 +0900

    달팽이 숫자

--- swea1954.py
+++ swea1954.py
@@ -1,1 +1,28 @@
-d
+t = int(input())
+for tc in range(t):
+  
+  n = int(input())
+  dist = [[0,1],[1,0],[0,-1],[-1,0]]
+  arr = [[0 for _ in range(n)] for _ in range(n)]
+
+  num = 1
+  d = 0
+  x,y = 0,-1
+  
+  while num <= n*n:
+    xidx, yidx = x+dist[d][0], y+dist[d][1]
+    if 0<=xidx<n and 0<=yidx<n and arr[xidx][yidx] == 0:
+      arr[xidx][yidx] = num
+      num += 1
+      x,y = xidx, yidx
+
+    else:
+      d = (d+1) % 4
+
+  print('#{}'.format(tc+1))
+
+  for i in arr:
+    print(*i)
+  
+
+  
